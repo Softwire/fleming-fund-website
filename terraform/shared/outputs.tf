3d0b0eee15cced4f850e44622e58fab6d5d55288
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2020-01-03 14:25:38 +0000

    add efs

--- terraform/shared/outputs.tf
+++ terraform/shared/outputs.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "elastic_beanstalk_application_name" {
   value = aws_elastic_beanstalk_application.app.name
 }
+
+output "vpc_public_subnet_id" {
+  value = aws_subnet.vpc-public-subnet.id
+}
