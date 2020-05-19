835d2d1b64deaa253a9fa9c2b07ecf6f5033eadc
Sarah Binney <sarah.binney@softwire.com>
2020-05-19 09:45:00 +0100

    Revert "FFWS-44: Add grant number boxes to fellowships page"
    This reverts commit f8dec3c7da24073e8d46ca0ae94164517617f265.

--- src/fleming-theme/page-fellowships.php
+++ src/fleming-theme/page-fellowships.php
@@ -28,7 +28,6 @@ function fleming_get_content() {
 
     process_flexible_content($fleming_content, $fleming_content['fields']['flexible_content']);
 
-    show_grant_numbers_for_page($fleming_content);
     get_case_studies_supporting_content($fleming_content);
 
     return $fleming_content;
